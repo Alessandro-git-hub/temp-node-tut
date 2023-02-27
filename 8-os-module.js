d23899106b348c107fda5aa67257890f2a66dfab
Alessandro-git-hub <121443837+Alessandro-git-hub@users.noreply.github.com>
2023-02-27 07:07:19 +0100

    first commit

---
+++ 8-os-module.js
@@ -0,0 +1,20 @@
+const  os = require("os")
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+
+// method return the system uptime in seconds
+console.log(`The system uptime is ${os.uptime} seconds`)
+
+//
+
+const currentOs = {
+    name: os.type(),
+    release :os.release(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem()
+}
+
+console.log(currentOs)
